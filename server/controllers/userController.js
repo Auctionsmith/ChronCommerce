db86e30059c112f99f870204c8ea84849d695ac9
Just Bobby <robertstipp17@gmail.com>
2023-10-21 12:46:54 -0700

    Add User Router and User Controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,21 @@
+const {User} = require('../db/models')
+
+const userController = {};
+
+userController.getUser = (req,res,next) => {
+  // req.getUser
+}
+
+userController.createUser = (req,res,next) => {
+
+}
+
+userController.editUser = (req, res, next) => {
+
+}
+
+userController.deleteUser = (req, res, next) => {
+
+}
+
+module.exports = userController
