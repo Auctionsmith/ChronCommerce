376fe0acc502e8b629f1756a67f376211c3f37cc
John Arroyo II <jtarroyo2@gmail.com>
2023-10-25 10:58:34 -0600

    s3 Bucket complete and integrated into createAuction as middleware 'upload' in  /middleware/uploadImage.js

--- server/routes/auctionRouter.js
+++ server/routes/auctionRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const auctionController = require("../controllers/auctionController");
-const upload = require("./imageRouter.js")
+const upload = require("../middleware/uploadImage")
+
 
 router.get("/", auctionController.getAllAuctions, (req, res) => {
   return res.status(200).json(res.locals.auctions);
@@ -9,7 +10,11 @@ router.get("/", auctionController.getAllAuctions, (req, res) => {
 router.get("/random", auctionController.getRandomAuction, (req,res)=>{
   return res.status(200).json(res.locals.auction)
 })
-router.post("/", auctionController.createAuction, (req, res) => {
+router.get("/random", auctionController.getRandomAuction, (req,res)=>{
+  return res.status(200).json(res.locals.auction)
+})
+
+router.get("/:id", auctionController.getAuction, (req, res) => {
   return res.status(200).json(res.locals.auction)
 });
 
@@ -19,6 +24,7 @@ router.post("/", upload.single('auctionImage'), auctionController.createAuction,
 });
 
 router.delete("/:id", auctionController.deleteAuction, (req, res) => {
+  // Add res.locals if needed
   return res.sendStatus(200)
 });
 
@@ -26,5 +32,13 @@ router.patch("/:id", auctionController.updateAuction, (req,res)=>{
   return res.status(200).json(res.locals.auction)
 })
 
+
+// router.patch("/:id", listingController.updateListing, (req, res) => {
+//   return res.status(200).json(res.locals.updatedListing);
+// });
+
+// router.delete("/:id", listingController.deleteListing, (req, res) => {
+//   return res.status(200).json(res.locals.deletedListing);
+// });
 
 module.exports = router;
