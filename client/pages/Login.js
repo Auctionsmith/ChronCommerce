24d94261a0cdc9be21f30defe2b59fb5ce467b3e
Nick Doolan <nickdoolan31@gmail.com>
2023-10-25 22:42:51 -0400

    woah

--- client/pages/Login.js
+++ client/pages/Login.js
@@ -2,14 +2,31 @@ import React from "react";
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link, Switch, useNavigate } from "react-router-dom";
 import SignUp from "../pages/SignUp";
+import axios from 'axios'
+import { getUserInfo } from '../slices/userSlice'
+import { useDispatch } from 'react-redux'
+import { useRef } from 'react'
 
 const Login = () => {
+  const dispatch = useDispatch()
+
+  const usernameRef = useRef(null)
+  const passwordRef = useRef(null)
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
+    let username = usernameRef.current.value
+    let password = passwordRef.current.value
+
+
+  axios.post('auth/login', {username: username, password: password})
+    .then(data=> console.log(data))
+    .then((data)=> dispatch(getUserInfo(data)))
+
+
   }
 
   return ( 
@@ -23,6 +40,7 @@ const Login = () => {
             type="text"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
+            ref={usernameRef}
           />
         </div>
         <div className="form-group">
@@ -31,6 +49,7 @@ const Login = () => {
             type="password"
             onChange={(e) => setPassword(e.target.value)}
             value={password}
+            ref={passwordRef}
           />
         </div>
         <button>Login</button>
