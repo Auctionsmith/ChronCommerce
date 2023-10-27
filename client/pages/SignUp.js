f13c430bdcfeef5bb795d300f12c8b9daacfeb2f
Just Bobby <robertstipp17@gmail.com>
2023-10-26 18:03:08 -0700

    Add Signup

--- client/pages/SignUp.js
+++ client/pages/SignUp.js
@@ -24,7 +24,7 @@ const SignUp = () => {
   const handleSubmit = async(e) =>{
     e.preventDefault()
   }
-  console.log('test')
+  
   return (
     <section className="signup-container">
     <article className="signup-box">
